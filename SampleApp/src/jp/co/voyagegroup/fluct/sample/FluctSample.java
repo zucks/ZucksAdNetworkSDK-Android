7c1ed781108161c9f62c5e49842cc0bf170df8a6
h-minamiohtsu <hiroshi_minamiohtsu@voyagegroup.com>
2014-08-29 23:43:08 +0900

    v3.2.0

--- SampleApp/src/jp/co/voyagegroup/fluct/sample/FluctSample.java
+++ SampleApp/src/jp/co/voyagegroup/fluct/sample/FluctSample.java
@@ -2,9 +2,6 @@ package jp.co.voyagegroup.fluct.sample;
 
 import jp.co.voyagegroup.android.fluct.jar.FluctInterstitial;
 import android.app.Activity;
-import android.content.pm.ApplicationInfo;
-import android.content.pm.PackageManager;
-import android.content.pm.PackageManager.NameNotFoundException;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -22,20 +19,7 @@ public class FluctSample extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fluct_sample);
 
-        ApplicationInfo appliInfo = null;
-        String interstitialMediaId = "";
-
-        try {
-            appliInfo = getPackageManager().getApplicationInfo(getPackageName(), PackageManager.GET_META_DATA);
-
-            Bundle metaDataBundle = appliInfo.metaData;
-            if (metaDataBundle != null
-                    && metaDataBundle.get("FLUCT_MEDIA_ID_FOR_INTERSTITIAL") != null) {
-                interstitialMediaId = metaDataBundle.get("FLUCT_MEDIA_ID_FOR_INTERSTITIAL").toString();
-            }
-        } catch (NameNotFoundException e) {}
-
-        mFluctInterstitial = new FluctInterstitial(getApplicationContext(), interstitialMediaId);
+        mFluctInterstitial = new FluctInterstitial(getApplicationContext());
         mFluctInterstitial.setFluctInterstitialCallback(mInterstitialCallback);
 
         Button showIntersitial = (Button)findViewById(R.id.showIntersitial);
